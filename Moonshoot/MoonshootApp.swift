b839b45f5f82df09a91fea214ad56a91c13fc8bb
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-03-06 15:35:01 +0400

    Initial Commit

---
+++ Moonshoot/MoonshootApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshootApp.swift
+//  Moonshoot
+//
+//  Created by aplle on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
